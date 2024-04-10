219a260a413f0caf8622d78fcdbeea4232364eeb
arushi <arushimadhesiya309@gmail.com>
2024-04-10 23:26:36 +0530

    with nanoid, basic complete

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,29 @@
 const express=require('express');
-
 const app=express();
+const http=require('http');
+const server=http.createServer(app);
+//const SocketIo=;
+const io=require('socket.io')(server,{
+    cors:{
+        origin:"*"
+    }
+});
+io.on('connection',(socket)=>{
+    console.log("connected");
+   // console.log("our socket while connection: ",socket);
+    // events will be handled
+    socket.on('chat',(payload)=>{
+        //console.log("payload when chat event emitted: ",socket);
+        //console.log("chat recieved");
+        io.emit("chat",payload)
+    })
+    socket.on("disconnect",(socket)=>{
+        console.log("disconnected");
+        console.log("our socket while disconnected: ",socket);
+    })
+});
 
-
-app.listen(5000,(err)=>{
+server.listen(5000,(err)=>{
     if(err){
         console.warn("errrrrrr");
     }
--- frontend/chitchat/src/App.js
+++ frontend/chitchat/src/App.js
@@ -1,11 +1,53 @@
-import logo from "./logo.svg";
+import React, { useEffect, useState } from 'react';
 import "./App.css";
+import io from 'socket.io-client';
+import { nanoid } from 'nanoid';
+
+const socket = io.connect("http://localhost:5000");
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [chat, setChat] = useState([]);
+  const [userName,setuserName]=useState(nanoid(4));
+  const send = (e) => {
+    e.preventDefault();
+    console.warn(message);
+    socket.emit('chat', { message,userName });
+    setMessage('');
+  };
+  useEffect(()=>{
+    socket.on('chat',(payload)=>{
+      console.log("chat in useefect")
+      setChat([...chat,payload])
+    })
+  })
   return (
     <div className="App">
       <header className="App-header">
         <div>ChitChat</div>
+        <div className='allchats'>
+          {
+            chat.map((payload,index)=>{
+              if(payload.userName==userName) return <p key={index} className="box-right">{payload.message}
+              <span> BY: {payload.userName}</span>
+              </p>
+              else return <p key={index} className="box-left">{payload.message}
+              <span> by: {payload.userName}</span>
+              </p>
+              
+            })
+          }
+        </div>
+        <form onSubmit={send}>
+          <input
+            type='text'
+            placeholder='type message'
+            value={message}
+            name='chat'
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button type='submit'>send</button>
+        </form>
       </header>
     </div>
   );
